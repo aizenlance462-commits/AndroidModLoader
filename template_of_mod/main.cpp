9865035454bb57e5bc71c9e3fee6bb84d6f4b625
aizenlance462-commits <aizenlance462@gmail.com>
2025-12-24 03:40:52 +0800

    Update main.cpp

--- template_of_mod/main.cpp
+++ template_of_mod/main.cpp
@@ -2,42 +2,68 @@
 #include <mod/logger.h>
 #include <mod/config.h>
 
-MYMODCFG(net.rusjj.mymod.guid, AML Mod Template, 1.0, RusJJ)
+// 1. SETUP NG MOD INFO
+MYMOD(com.madleg.ragdoll, RagdollPhysics, 1.0, Madleg_JuniorDjjr)
 
-//MYMOD(net.rusjj.mymod.guid, AML Mod Template Without Config, 1.0, RusJJ)
+// 2. GLOBAL VARIABLES
+Config* cfg = NULL;
 
-//NEEDGAME(net.rusjj.mygame)
+int iNumSubsteps, iNumIPLInstances;
+float fSimulationSpeed, fRagdollLinearDamping, fRagdollAngularDamping;
+float fRagdollGravity, fBulletPower, fExplosionPower;
+float fSurfaceFriction, fRagDollFriction, fRagDollRestitution;
+float fRagDollJointDamping, fRagDollGetUpThreshold;
+float fRagDollShapeMargin, fWorldShapeMargin, fDynamicObjectMass;
+float fWorldMinX, fWorldMinY, fWorldMaxX, fWorldMaxY;
+bool bVehicleShapeUseSpheres, bUseDynamicObjects;
 
-//BEGIN_DEPLIST()
-//    ADD_DEPENDENCY_VER(net.rusjj.aml, 1.0)
-//END_DEPLIST()
+// 3. MAIN FUNCTION
+extern "C" void OnModLoad() {
+    logger->SetTag("RagdollPhysics");
+    logger->Info("Ragdoll Mod Loading...");
 
-uintptr_t pGameLibrary = 0;
-ConfigEntry* pCfgMyBestEntry;
+    // PAGBASA NG INI
+    cfg = new Config("RagDoll_physics");
+    iNumSubsteps = cfg->GetInt("CONFIG", "iNumSubsteps", 5);
+    iNumIPLInstances = cfg->GetInt("CONFIG", "iNumIPLInstances", 100000);
+    fSimulationSpeed = cfg->GetFloat("CONFIG", "fSimulationSpeed", 1.0f);
+    fRagdollLinearDamping = cfg->GetFloat("CONFIG", "fRagdollLinearDamping", 0.05f);
+    fRagdollAngularDamping = cfg->GetFloat("CONFIG", "fRagdollAngularDamping", 0.8f);
+    fRagdollGravity = cfg->GetFloat("CONFIG", "fRagdollGravity", 1.0f);
+    fBulletPower = cfg->GetFloat("CONFIG", "fBulletPower", 1.0f);
+    fExplosionPower = cfg->GetFloat("CONFIG", "fExplosionPower", 1.2f);
+    fSurfaceFriction = cfg->GetFloat("CONFIG", "fSurfaceFriction", 0.9f);
+    fRagDollFriction = cfg->GetFloat("CONFIG", "fRagDollFriction", 0.9f);
+    fRagDollRestitution = cfg->GetFloat("CONFIG", "fRagDollRestitution", 0.1f);
+    fRagDollJointDamping = cfg->GetFloat("CONFIG", "fRagDollJointDamping", 10.0f);
+    fRagDollGetUpThreshold = cfg->GetFloat("CONFIG", "fRagDollGetUpThreshold", 0.5f);
+    fRagDollShapeMargin = cfg->GetFloat("CONFIG", "fRagDollShapeMargin", 0.02f);
+    fWorldShapeMargin = cfg->GetFloat("CONFIG", "fWorldShapeMargin", 0.01f);
+    fDynamicObjectMass = cfg->GetFloat("CONFIG", "fDynamicObjectMass", 20000.0f);
+    fWorldMinX = cfg->GetFloat("CONFIG", "fWorldMinX", -4000.0f);
+    fWorldMinY = cfg->GetFloat("CONFIG", "fWorldMinY", -4000.0f);
+    fWorldMaxX = cfg->GetFloat("CONFIG", "fWorldMaxX", 4000.0f);
+    fWorldMaxY = cfg->GetFloat("CONFIG", "fWorldMaxY", 4000.0f);
+    bVehicleShapeUseSpheres = cfg->GetBool("CONFIG", "bVehicleShapeUseSpheres", true);
+    bUseDynamicObjects = cfg->GetBool("CONFIG", "bUseDynamicObjects", true);
+    cfg->Save();
 
-extern "C" void OnModLoad()
-{
-    logger->SetTag("Mod Template");
-    
-    pGameLibrary = aml->GetLib("libMyGame.so");
-    if(pGameLibrary)
-    {
-        logger->Info("MyGame mod is loaded!");
+    // PAGBASA NG BIN
+    char path[256];
+    sprintf(path, "%s/RagDoll_physics.bin", aml->GetConfigPath()); 
+    FILE* file = fopen(path, "rb");
+    if (file) {
+        fseek(file, 0, SEEK_END);
+        long fileSize = ftell(file);
+        fseek(file, 0, SEEK_SET);
+        void* buffer = malloc(fileSize);
+        if (buffer) {
+            fread(buffer, fileSize, 1, file);
+            logger->Info("RagDoll_physics.bin loaded!");
+            free(buffer); 
+        }
+        fclose(file);
     }
-    else
-    {
-        logger->Error("MyGame mod is not loaded :(");
-        return; // Do not load our mod?
-    }
-
-    pCfgMyBestEntry = cfg->Bind("mySetting", "DefaultValue is 0?", "MyUniqueSection");
-    pCfgMyBestEntry->SetString("DefaultValue is unchanged");
-    pCfgMyBestEntry->SetInt(1);
-    pCfgMyBestEntry->Reset();
-    delete pCfgMyBestEntry; // Clean-up memory
-    
-    bool bEnabled = cfg->Bind("Enable", true)->GetBool();
-    delete Config::pLastEntry; // Clean-up of the latest ConfigEntry*
-    
-    cfg->Save(); // Will only save if something was changed
+    logger->Info("Ragdoll Mod initialization finished!");
 }
+
