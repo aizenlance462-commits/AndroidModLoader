4dc28a0cfd59a1c870169edcbcda7a0c3e274269
aizenlance462-commits <aizenlance462@gmail.com>
2025-12-24 04:23:50 +0800

    Update Android.mk

--- template_of_mod/Android.mk
+++ template_of_mod/Android.mk
@@ -1,10 +1,20 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_CPP_EXTENSION := .cpp .cc
-LOCAL_MODULE    := modtemplate
+
+# Ito ang magiging pangalan ng mod mo (lalabas na RagdollPhysics.so)
+LOCAL_MODULE    := RagdollPhysics
+
+# Dito nakalista ang lahat ng .cpp files na kailangan i-compile
+# Siguraduhin na tama ang path ng logger at config
 LOCAL_SRC_FILES := main.cpp mod/logger.cpp mod/config.cpp
-LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
+
+# Mga settings para sa compiler
+LOCAL_CFLAGS    += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
 LOCAL_C_INCLUDES += ./include
-LOCAL_LDLIBS += -llog
+
+# Importante ito para sa logging sa Android logcat
+LOCAL_LDLIBS    += -llog
+
 include $(BUILD_SHARED_LIBRARY)
+
